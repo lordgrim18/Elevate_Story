6d234d1dcc240ebff029132314e36bdb29cc7ae7
lordgrim18 <jeromjomanthara@gmail.com>
2023-09-25 04:29:56 +0530

    updated scrapper

--- scrapper.py
+++ scrapper.py
@@ -48,58 +48,114 @@ def main():
         fixed_url_list.append(BASE_URL + book_url)
 
     all_books_url = []
-    for book_url in fixed_url_list:
-        driver.get(book_url)
-        random_sleep()
-        soup = get_soup(driver)
-        books_div_list = soup.find_all('div', class_='header module')
-        for books_div in books_div_list:
-            book_url = books_div.find('a')['href']
-            all_books_url.append(book_url)
-
     fixed_all_books_url = []
-    for book_url in all_books_url:
-        fixed_all_books_url.append(BASE_URL + book_url)
 
-        # Open the CSV file for writing
+    # for book_url in fixed_url_list:
+    #     print(len(all_books_url))
+    #     if (len(all_books_url) < 50):
+    #         driver.get(book_url)
+    #         random_sleep()
+    #         soup = get_soup(driver)
+    #         books_div_list = soup.find_all('div', class_='header module')
+    #         for books_div in books_div_list:
+    #             book_url = books_div.find('a')['href']
+    #             fixed_all_books_url.append(BASE_URL + book_url)
+    #     else :
+    #         break
+
+    # for book_url in all_books_url:
+    #     fixed_all_books_url.append(BASE_URL + book_url)
+
+    index = 1
     with open(CSV_FILE, mode='w', newline='', encoding='utf-8') as csv_file:
         csv_writer = csv.writer(csv_file)
         # Write CSV header
         csv_writer.writerow(['URL', 'Title', 'Content', 'Rating', 'Additional Tags'])
+        for book_url in fixed_url_list:
+            print(len(all_books_url))
+            driver.get(book_url)
+            random_sleep()
+            soup = get_soup(driver)
+            books_div_list = soup.find_all('div', class_='header module')
+            for books_div in books_div_list:
+                book_url = books_div.find('a')['href']
+                url = BASE_URL + book_url
+                if url:
+                    driver.get(url)
+                    random_sleep()
+                    soup = get_soup(driver)
+
+                    try:
+                        title = soup.find('h2', class_='title heading').text.strip()
+                    except:
+                        title = 'Not Found'
+
+                    try:
+                        content_element = soup.find('div', class_='userstuff module')
+                        content = content_element.text.strip()
+                    except:
+                        content = 'Not Found'
+
+                    try:
+                        rating_element = soup.find('dd', class_='rating tags')
+                        rating = rating_element.text.strip()
+                    except:
+                        rating = 'Not Found'
+
+                    try:
+                        additional_tags_element = soup.find('dd', class_='freeform tags')
+                        additional_tags = additional_tags_element.text.strip()
+                    except:
+                        additional_tags = 'Not Found'
+
+                    # Write product information to the CSV file
+                    csv_writer.writerow([url, title, content, rating, additional_tags])
+                    print(index)
+                    index = index + 1
+
+    #     # Open the CSV file for writing
+    # with open(CSV_FILE, mode='w', newline='', encoding='utf-8') as csv_file:
+    #     csv_writer = csv.writer(csv_file)
+    #     # Write CSV header
+    #     csv_writer.writerow(['URL', 'Title', 'Content', 'Rating', 'Additional Tags'])
         
-        # Iterate through each product and extract information
-        for index, url in enumerate(fixed_all_books_url, start=1):
-            if url:
-                driver.get(url)
-                random_sleep()
-                soup = get_soup(driver)
+    #     # Iterate through each product and extract information
+    #     for index, url in enumerate(fixed_all_books_url, start=1):
+    #         if url:
+    #             driver.get(url)
+    #             random_sleep()
+    #             soup = get_soup(driver)
 
-                try:
-                    title = soup.find('h2', class_='title heading').text.strip()
-                except:
-                    title = 'Not Found'
+    #             try:
+    #                 title = soup.find('h2', class_='title heading').text.strip()
+    #             except:
+    #                 title = 'Not Found'
 
-                try:
-                    content_element = soup.find('div', class_='userstuff module')
-                    content = content_element.text.strip()
-                except:
-                    content = 'Not Found'
+    #             try:
+    #                 content_element = soup.find('div', class_='userstuff module')
+    #                 content = content_element.text.strip()
+    #             except:
+    #                 content = 'Not Found'
 
-                try:
-                    rating_element = soup.find('dd', class_='rating tags')
-                    rating = rating_element.text.strip()
-                except:
-                    rating = 'Not Found'
+    #             try:
+    #                 rating_element = soup.find('dd', class_='rating tags')
+    #                 rating = rating_element.text.strip()
+    #             except:
+    #                 rating = 'Not Found'
 
-                try:
-                    additional_tags_element = soup.find('dd', class_='freeform tags')
-                    additional_tags = additional_tags_element.text.strip()
-                except:
-                    additional_tags = 'Not Found'
+    #             try:
+    #                 additional_tags_element = soup.find('dd', class_='freeform tags')
+    #                 additional_tags = additional_tags_element.text.strip()
+    #             except:
+    #                 additional_tags = 'Not Found'
 
-                # Write product information to the CSV file
-                csv_writer.writerow([url, title, content, rating, additional_tags])
-                print(index)
+    #             # Write product information to the CSV file
+    #             csv_writer.writerow([url, title, content, rating, additional_tags])
+    #             print(index)
+
+
+
+
 
     print(f"Data written to {CSV_FILE}")
     driver.quit()
